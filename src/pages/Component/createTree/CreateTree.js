80cb564ab5a2ba10b00f697798352c8cc844db15
optigo-app <130429763+optigo-app@users.noreply.github.com>
2023-12-20 14:08:04 +0530

    chnage the home end createTree page

--- src/pages/Component/createTree/CreateTree.js
+++ src/pages/Component/createTree/CreateTree.js
@@ -5,138 +5,129 @@ import './CreateTree.css'
 import { IoMdClose } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
 
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { makeStyles } from '@mui/styles';
+import { TextField } from '@mui/material';
+
+const useStyles = makeStyles({
+    datePickerRoot: {
+        width: '100%',
+        marginTop: '10px',
+        //   height: '20%'
+    },
+    datePickerInput: {
+        backgroundColor: 'lightgray',
+        borderRadius: '4px',
+        padding: '8px',
+        width: '100%',
+        height: '20px'
+    },
+
+    inputRoot: {
+        color: 'blue', // Change color to desired value
+        '&::placeholder': {
+            color: 'red', // Change placeholder color to desired value
+        },
+    },
+
+});
+
+
 export default function QRScanner() {
-    const [scannedResult, setScannedResult] = useState('');
-    const [inputValue, setInputValue] = useState('');
+    const [inputValue, setInputValue] = useState(undefined);
     const [enteredValues, setEnteredValues] = useState([]);
+    const [inputError, setInputError] = useState(false)
     const navigation = useNavigate();
+    const classes = useStyles();
+
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
 
     const handleGoButtonClick = () => {
-        setEnteredValues([...enteredValues, inputValue]); // Add the input value to the entered values array
-        setInputValue(''); // Clear the input field after adding the value
+        if (inputValue === '' || inputValue === undefined) {
+            setInputError(true)
+        } else {
+            setInputError(false)
+            setEnteredValues([...enteredValues, inputValue]); // Add the input value to the entered values array
+            setInputValue(''); // Clear the input field after adding the value
+        }
+
     };
 
     const totalValues = enteredValues.length;
-    const handleScan = (data) => {
-        if (data) {
-            setScannedResult(data);
-        }
-    };
-
-    const handleError = (err) => {
-        console.error(err);
-    };
 
     const handleRemoveItem = (indexToRemove) => {
         setEnteredValues(enteredValues.filter((_, index) => index !== indexToRemove));
     };
 
-    const [image, setImage] = useState(null);
-
-    const handleImageUpload = (e) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-
-        reader.onloadend = () => {
-            setImage(reader.result);
-        };
-
-        if (file) {
-            reader.readAsDataURL(file);
-        }
-    };
-
-
     return (
-        <div>
-            <p className='mainTitle' >Create New Batch</p>
-            <div className='Header'>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    <button className='headerTwoListBtn' onClick={() => navigation('/addFlask')}>New Tree</button>
-                    <button className='headerTwoListBtn'>Show List</button>
-                </div>
-            </div>
 
-            {/* <QrReader
-                delay={300}
-                onError={handleError}
-                onScan={handleScan}
-                style={{ width: '100%' }}
-            />
-            <p>Scanned Result: {scannedResult}</p> */}
+        <div>
+            {/* <button className='headerTwoListBtn' onClick={() => navigation('/addFlask')}>New Tree</button> */}
+            <p className='mainTitle' >PROCASTING CREATE NEW BATCH</p>
             <div style={{ display: 'flex', marginTop: '30px' }}>
                 <div className='allDataCreteDiv'>
-                    <div style={{ display: 'flex', marginLeft: '1%' }}>
-                        <div className='createInfoMain'>
-                            <p className='infoTextTitle'>Date  :</p>
-                            <input type='text' className='infoTextInput' />
+                    <div style={{ display: 'flex' }}>
+                        <input type='text' placeholder='Batch' className='infoTextInput' />
+                        <input type='text' placeholder='Enter Weight' className='infoTextInput' />
+                        <input type='text' placeholder='Eneter Assign To' value={'E0025(ANDERSON PATRICK)'} className='infoTextInput' />
+                    </div>
+                    <div style={{ display: 'flex', paddingInline: '10px' }}>
+                        <div style={{ width: '50%' }}>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker
+                                    className={classes.datePickerRoot}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params.inputProps}
+                                            placeholder="Choose date" // Placeholder text
+                                            InputLabelProps={{
+                                                shrink: true,
+                                            }}
+                                        />
+                                    )}
+                                />
+                            </LocalizationProvider>
                         </div>
-
-                        <div className='createInfoMain'>
-                            <p className='infoTextTitle'>Batch  :</p>
-                            <input type='text' placeholder='Batch' className='infoTextInput' />
+                        <div style={{ width: '50%' }}>
+                            <select className='selecGold'>
+                                <option className='selecGoldOption'>GOLD 14K WHITE</option>
+                                <option className='selecGoldOption'>GOLD 14K YELLOW</option>
+                                <option className='selecGoldOption'>GOLD 18K WHITE</option>
+                                <option className='selecGoldOption'>GOLD 18K YELLOW</option>
+                            </select>
                         </div>
                     </div>
-                    <div style={{ display: 'flex', marginLeft: '1%' }}>
-                        <div className='createInfoMain'>
-                            <p className='infoTextTitle'>Weight  :</p>
-                            <input type='text' className='infoTextInput' />
-                        </div>
-
-                        <div className='createInfoMain'>
-                            <p className='infoTextTitle'>Assign To  :</p>
-                            <input type='text' className='infoTextInput' />
-                        </div>
-                    </div>
-                    <div style={{ display: 'flex', marginLeft: '1%' }}>
-                        <div className='createInfoMain'>
-                            <p className='infoTextTitle'>Metal Color:</p>
-                            <input type='text' className='infoTextInput' />
-                        </div>
-                        <div className='createInfoMain'>
-                            <p className='infoTextTitle'>Metal Type :</p>
-                            <input type='text' className='infoTextInput' />
-                        </div>
-                    </div>
-                </div>
-                <div className='uploadBtnMain'>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        capture="camera"
-                        onChange={handleImageUpload}
-                    />
-                    {/* <button className='uploadTreeBtn'>Upload Tree Image</button> */}
-                    <button className='printQRbtn'>Print QR</button>
                 </div>
             </div>
-            <div style={{ display: 'flex',marginTop: '20px' }}>
-                <div style={{ display: 'flex', margin: '60px', width: '30%', flexDirection: 'column' }}>
-                    <QRCode value='Scan a QR code' />
-                    <div style={{ marginTop: '20px' }}>
-                        <input type='text' className='enterBrachItemBox' value={inputValue}
+            <div style={{ display: 'flex', marginTop: '20px' }}>
+                <div className='createORMain'>
+                    <QRCode value='Scan a QR code' style={{ height: '200px', width: '200px' }} />
+                    <div style={{display:'flex', marginTop: '20px' }}>
+                        <input type='text' style={{ border: inputError && '1px solid red' }} className='enterBrachItemBox' value={inputValue}
                             onChange={handleInputChange} />
-                        <button style={{ height: '35px', cursor: 'pointer' }} onClick={handleGoButtonClick}>
+                        <button style={{ height: '45px', width: '50px', fontSize: '20px', fontWeight: 600, cursor: 'pointer' }} onClick={handleGoButtonClick}>
                             Go
                         </button>
-
                     </div>
-
                     <button className='showInfoBtn'>Show Info</button>
-
+                    {/* printQRbtn */}
+                    <button className='showInfoBtn'  onClick={() => navigation('/addFlask')}>print OR</button>
+                    <button className='showInfoBtn'  onClick={() => navigation('/addFlask')}>Show list</button>
+                    <button className='showInfoBtn'  onClick={() => navigation('/addFlask')}>uppload tree image</button>
                 </div>
                 <div className='allScaneDataMain'>
                     <p className='totalItemText'>{totalValues} Item Added</p>
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ height: '540px', overflow: 'scroll', display: 'flex', flexDirection: 'column' }}>
                         {enteredValues.map((value, index) => (
-                            <ul className='allScanDataUl'>
-                                <li className='allScanDataLi' key={index}>{value}</li>
-                                <IoMdClose style={{ height: '25px', width: '25px', cursor: 'pointer' }} onClick={() => handleRemoveItem(index)} />
-                            </ul>
+                            <div className='allScandataMain' >
+                                <p className='allScanData' key={index}>{value}</p>
+                                <IoMdClose style={{ height: '40px', color: 'red', width: '40px', cursor: 'pointer' }} onClick={() => handleRemoveItem(index)} />
+                            </div>
                         ))}
                     </div>
                 </div>
